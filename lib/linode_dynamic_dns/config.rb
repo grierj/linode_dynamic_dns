026115efaf1def2c4e1ac331aadc13d0b924408b
Grier Johnson <grierj@gmail.com>
2014-05-08 16:44:41 -0700

    Adding ability to specify config Making default mode silent and adding verbose mode

--- lib/linode_dynamic_dns/config.rb
+++ lib/linode_dynamic_dns/config.rb
@@ -3,13 +3,23 @@ require 'yaml'
 
 module LinodeDynamicDns
   def self.config
-    @config ||= OpenStruct.new(
-                  YAML.load_file(
-                    File.join(
-                      File.dirname(__FILE__), '../..', 'config',
-                      'linode_dynamic_dns.yaml'
-                    )
-                  )
-                )
+    @config ||= load_config_from(
+      File.join(
+        File.dirname(__FILE__), '../..', 'config',
+        'linode_dynamic_dns.yaml'
+      )
+    )
+  end
+
+  def self.load_config_from(path)
+    OpenStruct.new(
+      YAML.load_file(
+        path
+      )
+    )
+  end
+
+  def self.set_config(path)
+    @config = load_config_from(File.expand_path(path))
   end
 end
